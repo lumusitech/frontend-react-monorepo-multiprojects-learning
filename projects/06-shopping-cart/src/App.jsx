f3d2d3e20f7a1b348a5cb9d2e9b49496ae0729b4
Luciano Figueroa <lumusitech@gmail.com>
2025-05-19 20:27:14 -0300

    feat: add base product list with styles and pre filter feat

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from 'react'
+import { Products } from './components/Products'
+import { products as initialProducts } from './mocks/products.json'
+
 function App() {
+  const [products, setProducts] = useState([...initialProducts])
+  const [filters, setFilters] = useState({
+    category: 'groceries',
+    price: 10,
+  })
+
+  const filterProducts = (products = []) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.price &&
+        (filters.category === 'all' || product.category === filters.category)
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
     <>
       <h1>App</h1>
+      <Products products={filteredProducts} />
     </>
   )
 }
